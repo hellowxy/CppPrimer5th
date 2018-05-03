5c9dd3f9e34f3f020d9e8c67634d3652902cd5c1
lukexywang <wangxiaoying_op@163.com>
2018-05-03 09:46:24 +0800

    demo Please enter the commit message for your changes. Lines starting

---
+++ Chapter13/HasPtr.h
@@ -0,0 +1,24 @@
+#ifndef HASPTR_H
+#define HASPTR_H
+#include <string>
+
+class HasPtr{
+    private:
+        std::string *ps;
+        int i;
+    public:
+        HasPtr(const std::string &s = std::string()):ps(new std::string(s)), i(0){}
+        HasPtr(const HasPtr& other):ps(new std::string(pther->ps)),i(other.i)
+        {}
+        
+        HasPtr& operator=(const HasPtr& right){
+            if(this != &right){
+                delete ps;
+                //切记，要释放掉原来的ps指针!!!
+                this->ps = new string(*(right.ps));
+                this.i = right.i;
+            }
+            return *this;
+        }
+}
+#endif
